e2f5b409d9e8cf3a6efc792ff529aba965b56b45
RizqiKamall <rizqikamal0014@gmail.com>
2025-01-19 01:49:51 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 chmod +x *
-./hellminer -c stratum+ssl://na.luckpool.net:3958 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.Dudung1 -p x --cpu 6
+./hellminer -c stratum+ssl://na.luckpool.net:3958 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.Dudung1 -p x --cpu 4
